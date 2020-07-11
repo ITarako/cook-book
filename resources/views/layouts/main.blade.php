5b5fcea9089fdff5bccc9d53a43b154903634986
Igor Tarasenko <i.tarasenko@freematiq.com>
2020-07-12 01:37:19 +0700

    blade laravel/ui

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -11,7 +11,7 @@
         <link rel="shortcut icon" href="/img/favicon/favicon.ico">
         <meta name="theme-color" content="#ffffff">
 
-        <title>Laravel</title>
+        <title>Рассказы хомячка</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -72,12 +72,12 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
+            @if (true)
                 <div class="top-right links">
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else
-                        <a href="{{ route('login') }}">Login</a>
+                        <a href="http://cook-book.local/login">Login</a>
 
                         @if (Route::has('register'))
                             <a href="{{ route('register') }}">Register</a>
@@ -87,20 +87,7 @@
             @endif
 
             <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                @yield('content')
             </div>
         </div>
     </body>
