3f142ad9ef4184d14ea587481b8da37f9801b94c
Igor Tarasenko <i.tarasenko@freematiq.com>
2020-07-12 02:22:05 +0700

    подключение ресурсов в  шаблонах

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -10,65 +10,10 @@
         <link rel="manifest" href="/img/favicon/site.webmanifest">
         <link rel="shortcut icon" href="/img/favicon/favicon.ico">
         <meta name="theme-color" content="#ffffff">
-
         <title>Рассказы хомячка</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
         <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
+        @yield('styles')
     </head>
     <body>
         <div class="flex-center position-ref full-height">
@@ -90,5 +35,7 @@
                 @yield('content')
             </div>
         </div>
+        <script src="{{ mix('/js/app.js') }}"></script>
+        @yield('scripts')
     </body>
 </html>
