82bdc31623b12f1768c407da28a73e48678de23c
Igor Tarasenko <i.tarasenko@freematiq.com>
2020-07-13 23:16:33 +0700

    сгенерированный контроллера

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\Recipe;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
 use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\View\View;
 
 /**
  * Class RecipeController
@@ -11,37 +15,64 @@ use Illuminate\Http\Request;
  */
 class RecipeController extends Controller
 {
+    /**
+     * @return Application|Factory|View
+     */
     public function index()
     {
         return view('recipe.index');
     }
 
-    public function show(Recipe $recipe)
-    {
-        return view('recipe.show');
-    }
-
+    /**
+     * @return Application|Factory|View
+     */
     public function create()
     {
         return view('recipe.create');
     }
 
-    public function store()
+    /**
+     * @param Request $request
+     * @return RedirectResponse
+     */
+    public function store(Request $request)
     {
         return redirect()->route('recipe.index');
     }
 
+    /**
+     * @param Recipe $recipe
+     * @return Application|Factory|View
+     */
+    public function show(Recipe $recipe)
+    {
+        return view('recipe.show');
+    }
+
+    /**
+     * @param Recipe $recipe
+     * @return Application|Factory|View
+     */
     public function edit(Recipe $recipe)
     {
         return view('recipe.edit');
     }
 
-    public function update(Recipe $recipe)
+    /**
+     * @param Request $request
+     * @param Recipe $recipe
+     * @return RedirectResponse
+     */
+    public function update(Request $request, Recipe $recipe)
     {
         return redirect()->route('recipe.show');
     }
 
-    public function delete(Recipe $recipe)
+    /**
+     * @param Recipe $recipe
+     * @return RedirectResponse
+     */
+    public function destroy(Recipe $recipe)
     {
         return redirect()->route('recipe.index');
     }
