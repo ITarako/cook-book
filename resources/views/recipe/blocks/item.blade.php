ddf9a4f11ad7c5b545e2367b400f95f094fbdb35
Igor Tarasenko <i.tarasenko@freematiq.com>
2020-08-08 13:39:35 +0700

    докер

--- resources/views/recipe/blocks/item.blade.php
+++ resources/views/recipe/blocks/item.blade.php
@@ -3,6 +3,13 @@
     <th>{{ $model->category->name }}</th>
     <th>
         <a
+            class="btn btn-sm btn-outline-primary"
+            href="{{route('recipe.show', $model)}}"
+            title="Просмотр"
+        >
+            <i class="fas fa-eye"></i>
+        </a>
+        <a
             class="btn btn-sm btn-primary"
             href="{{route('recipe.edit', $model)}}"
             title="Редактировать"
